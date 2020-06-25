da69dcb3ea1a5e33b50a149f24ec445145a1ccb8
AquaticPyro <aquaticpyro@gmail.com>
2020-06-25 08:31:51 +0100

    Stand in login page set up and a link added from the homepage to get there easily

--- expenses_app/__init__.py
+++ expenses_app/__init__.py
@@ -5,5 +5,5 @@ from os.path import abspath
 def create_app():
     app = Flask(__name__, template_folder="templates")
     with app.app_context():
-        from expenses_app.views import index
+        from expenses_app.views import index, login
     return app
--- expenses_app/views.py
+++ expenses_app/views.py
@@ -4,3 +4,8 @@ from flask import current_app as app, render_template
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
