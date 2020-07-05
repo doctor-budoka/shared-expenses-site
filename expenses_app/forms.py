a48ef0078897d89123f74d07322c6fd0681f1cc1
AquaticPyro <aquaticpyro@gmail.com>
2020-07-05 13:29:10 +0100

    Added a registration view and template

--- expenses_app/forms.py
+++ expenses_app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import InputRequired, Email
+from wtforms.validators import InputRequired, Email, EqualTo
 
 
 class LogInForm(FlaskForm):
@@ -8,4 +8,16 @@ class LogInForm(FlaskForm):
         InputRequired(message="You must provide an email address to continue"),
         Email(message="Email entered is not a valid email address")])
     password = PasswordField("Password", [InputRequired(message="You must provide a password to continue")])
+    submit = SubmitField("Submit")
+
+
+class Register(FlaskForm):
+    email = StringField("Email", [
+        InputRequired(message="You must provide an email address to continue"),
+        Email(message="Email entered is not a valid email address")])
+    password = PasswordField("Password", [InputRequired(message="You must provide a password to continue")])
+    confirm = PasswordField("Confirm", [
+        InputRequired(message="You must provide a password to continue"),
+        EqualTo("password", message="Password and confirmation must be the same!")
+    ])
     submit = SubmitField("Submit")
--- expenses_app/views.py
+++ expenses_app/views.py
@@ -1,5 +1,5 @@
 from flask import current_app as app, render_template, redirect, url_for
-from expenses_app.forms import LogInForm
+from expenses_app.forms import LogInForm, Register
 
 
 @app.route("/")
@@ -13,3 +13,11 @@ def login():
     if form.validate_on_submit():
         return redirect(url_for("index"))
     return render_template("login.html", form=form)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = Register()
+    if form.validate_on_submit():
+        return redirect(url_for("index"))
+    return render_template("register.html", form=form)
