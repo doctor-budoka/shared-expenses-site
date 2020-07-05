36e4f8eff7fb294c00407f868cb24011d3864f1b
AquaticPyro <aquaticpyro@gmail.com>
2020-07-05 14:13:03 +0100

    Added some methods for handling passwords

--- expenses_app/models.py
+++ expenses_app/models.py
@@ -1,6 +1,7 @@
 import datetime as dt
 
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -14,8 +15,16 @@ class AuthorisedEmail(db.Model):
 
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email_id = db.Column(db.Integer, db.ForeignKey("authorised_email.email_id"))
+    email_id = db.Column(db.Integer, db.ForeignKey("authorised_email.email_id"), unique=True, index=True)
     email = db.relationship("AuthorisedEmail", back_populates="user")
     password_hash = db.Column(db.String(128))
     time_joined = db.Column(db.DateTime, default=dt.datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
