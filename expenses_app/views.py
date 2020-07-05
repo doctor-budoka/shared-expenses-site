647a8fb6cb9cd0a13816d497905e75fbd05197c0
AquaticPyro <aquaticpyro@gmail.com>
2020-07-05 13:23:22 +0100

    login route updated to handle the form

--- expenses_app/views.py
+++ expenses_app/views.py
@@ -1,4 +1,5 @@
-from flask import current_app as app, render_template
+from flask import current_app as app, render_template, redirect, url_for
+from expenses_app.forms import LogInForm
 
 
 @app.route("/")
@@ -6,6 +7,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    form = LogInForm()
+    if form.validate_on_submit():
+        return redirect(url_for("index"))
+    return render_template("login.html", form=form)
