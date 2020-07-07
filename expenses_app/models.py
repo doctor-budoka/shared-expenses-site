5ad42e3d39dae5fcdb8cb9c09031b083d7021588
AquaticPyro <aquaticpyro@gmail.com>
2020-07-07 09:24:58 +0100

    Added Group to models and added membership and ownership relationships

--- expenses_app/models.py
+++ expenses_app/models.py
@@ -20,6 +20,16 @@ class AuthorisedEmail(db.Model):
         self.is_registered = True
         return True
 
+    def __repr__(self):
+        return f"<AuthEmail {self.email}>"
+
+
+group_membership_table = db.Table(
+    "group_membership", db.Base.metadata,
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("group_id", db.Integer, db.ForeignKey("group.id"))
+)
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -33,6 +43,8 @@ class User(UserMixin, db.Model):
     email = db.relationship("AuthorisedEmail", back_populates="user")
     password_hash = db.Column(db.String(128), nullable=False)
     time_joined = db.Column(db.DateTime, default=dt.datetime.utcnow)
+    owned_groups = db.relationship("Group", backpopulates="owner")
+    groups = db.relationship("Groups", secondary=group_membership_table, back_populates="members")
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -50,5 +62,23 @@ class User(UserMixin, db.Model):
         else:
             return None
 
+    def create_group(self, name):
+        new_group = Group()
+        new_group.name = name
+        new_group.owner = self
+        self.groups.append(new_group)
+
     def __repr__(self):
         return f"<User {self.username}>"
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, index=True, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey("user.id"), index=True, nullable=False)
+    owner = db.relationship("User", backpopulates="owned_groups")
+
+    members = db.relationship("User", secondary=group_membership_table, back_populates="groups")
+
+    def __repr__(self):
+        return f"<Group {self.id}, {self.name}>"
