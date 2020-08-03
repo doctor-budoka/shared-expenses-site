7b9d778a8b51a31611e084fc6582677a78d7b789
j-boland <james@jboland.tech>
2020-08-03 16:31:29 +0100

    Bash script for setting up test users created

---
+++ test-user-setup.sh
@@ -0,0 +1,4 @@
+flask reset-db
+flask create-auth-emails example@example.com user-one@gmail.com another@user.com
+flask create-user new-user password example@example.com
+flask create-user other-user password user-one@gmail.com
