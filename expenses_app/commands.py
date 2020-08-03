632fdf4d9ae81baf18fc503fcea95d7dc2874f8f
j-boland <james@jboland.tech>
2020-08-03 16:08:57 +0100

    Added logging and improved naming in commands.py

--- expenses_app/commands.py
+++ expenses_app/commands.py
@@ -14,12 +14,15 @@ def reset_db():
 
 @app.cli.command("create-auth-emails")
 @click.argument("emails", nargs=-1)
-def load_authorised_emails(emails):
+def create_authorised_emails(emails):
     """Adds emails to the Authorised emails db"""
+    click.echo("Emails added to authorised_email:")
     if emails:
         for email in emails:
+            click.echo(f"\t'{email}'")
             new_email = AuthorisedEmail()
             new_email.email = email
+            db.session.add(new_email)
         db.session.commit()
 
 
@@ -27,8 +30,12 @@ def load_authorised_emails(emails):
 @click.argument("username")
 @click.argument("password")
 @click.argument("email")
-def load_user(username, password, email):
+def create_user(username, password, email):
     """Adds a user to the db"""
-    auth_email = AuthorisedEmail.query.filter_by(email=email)
-    User.create_user(auth_email, password, username)
-    db.session.commit()
+    click.echo(f"Creating user with email='{email}', username='{username}'")
+    auth_email = AuthorisedEmail.query.filter_by(email=email).first()
+    if auth_email:
+        User.create_user(auth_email, password, username)
+        db.session.commit()
+    else:
+        raise ValueError(f"{email} is not an authorised email address!")
